a5aa1c0bd165b37c8b53d5a315869f8b817b2bb9
Hugo <hugo.resina.7e7@itb.cat>
2026-02-27 17:00:20 +0100

    save progress

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Node : MonoBehaviour
 {
-    public float nodeX, nodeY;
+    //public float nodeX, nodeY;
     public bool hasBeenVisited;
     public int Heuristic;
+    public int id;
+    public SpriteRenderer _spriteRenderer;
 
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void asdf()
+    {
+        Debug.Log("adsfasdf");
+    }
 
 }
--- Assets/PathFindingManager.cs
+++ Assets/PathFindingManager.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class PathFindingManager : MonoBehaviour
 {
-    private int Size = 10;
-    public int Dimension = 20;
+    
+    private int Dimension = 10;
     public int Center;
     public Node Node;
     public Node[,] Map;
+    public Camera mainCamera;
     private void Start()
     {
         Center = (Dimension / 2) - Dimension;
         Map = new Node[Dimension, Dimension];
 
         StartMap();
+
+        
     }
     public void StartMap()
     {
@@ -22,13 +25,35 @@ public class PathFindingManager : MonoBehaviour
         {
             for(int j = 0; j < Dimension; j++)
             {
-                Instantiate(Node);
-                Node.transform.position = new Vector2(Center + i, Center + j);
-              
+                Node CurrNode = Instantiate(Node);
+                CurrNode.transform.position = new Vector2(Center + i * 1.05f , Center + j * 1.05f );
+                CurrNode.id = i*Dimension + j;
+                //Debug.Log(CurrNode.id);
                 Map[i, j] = Node;
-
-                
             }
         }
     }
+
+    
+    public void RedrawMap(Node[,] Map)
+    {
+        for (int i = 0; i < Map.Length; i++)
+        {
+            for (int j = 0; j < Map.Length; j++)
+            {
+                Map[i, j].hasBeenVisited = true;
+                SetState(Map[i, j]);
+            }
+        }
+        
+    }
+    public void SetState(Node node)
+    {
+        if (node.hasBeenVisited)
+        {
+            node._spriteRenderer.color = Color.green;
+        }
+        Thread.Sleep(100);
+    }
+    
 }
